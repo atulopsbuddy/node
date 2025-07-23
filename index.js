a62ab7376dd8760e83ab56a8e3f25ef7d6c24dc8
Atul Patil <113100006+atulopsbuddy@users.noreply.github.com>
2025-07-23 11:07:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+const { exec } = require("child_process");
+
+// ❌ Hardcoded secret (bad practice)
+const API_KEY = "12345-SECRET-KEY";
+
+// ❌ Command Injection (bad practice)
+const userInput = process.argv[2];
+exec(`ls ${userInput}`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
